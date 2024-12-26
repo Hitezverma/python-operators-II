f2d2e28b46c69e5982ff18a9cae791fc54a94ba2
Hitezverma <hitez.verma@education.nsw.gov.au>
2024-12-26 20:11:18 +1100

    ai

---
+++ A1.py
@@ -0,0 +1,14 @@
+a = 10
+b = 12
+c = 0 
+if a and b and c:
+    print("all numbers that are greater than booleon value as True")
+else:
+    print("Atleast one number has booleon value as False") 
+
+a = 10
+b = -10
+if a > 0 or b > 0:
+    print("Either number is greater than 0")
+else:
+    print("no number is greater than 0")    
